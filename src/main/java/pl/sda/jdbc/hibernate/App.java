f1de73a36d4ec0a57fab634417f1d62e6fc6818b
PawelKonczalski <konczalski.pawel@gmail.com>
2019-08-10 21:12:03 +0200

    hibernate no xml config

--- src/main/java/pl/sda/jdbc/hibernate/App.java
+++ src/main/java/pl/sda/jdbc/hibernate/App.java
@@ -1,13 +1,27 @@
 package pl.sda.jdbc.hibernate;
 
+import org.hibernate.Session;
+import pl.sda.jdbc.hibernate.config.HibernateUtil;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        String sql = "select version()";
+
+        String result = (String) session.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+
+        session.getTransaction().commit();
+        session.close();
+
+
+        HibernateUtil.shutdown();
     }
 }
---
+++ src/main/java/pl/sda/jdbc/hibernate/config/HibernateUtil.java
@@ -0,0 +1,54 @@
+package pl.sda.jdbc.hibernate.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HibernateUtil {
+    private static StandardServiceRegistry registry;
+    private static SessionFactory session;
+
+    public static SessionFactory getSessionFactory(){
+        if(session == null){
+            try {
+                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+                Map<String, String> settings = new HashMap<>();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3307/employee_db?useTimezone=true&serverTimezone=UTC");
+                settings.put(Environment.USER, "hbstudent");
+                settings.put(Environment.PASS, "hbstudent");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+                registryBuilder.applySettings(settings);
+
+                registry = registryBuilder.build();
+
+                MetadataSources sources = new MetadataSources(registry);
+
+                Metadata metadata = sources.getMetadataBuilder().build();
+
+                session = metadata.getSessionFactoryBuilder().build();
+            } catch (Exception e){
+                e.printStackTrace();
+                if (registry != null){
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return session;
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+}
